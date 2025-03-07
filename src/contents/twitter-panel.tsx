c2a24a0bd356439d88651bb0d7f179658108650f
luykin <luo530366891@gmail.com>
2025-03-07 22:11:39 +0800

    插入dom研究

--- src/contents/twitter-panel.tsx
+++ src/contents/twitter-panel.tsx
@@ -5,7 +5,7 @@ import cssText from 'data-text:~/css/style.css'
 import tipCss from 'data-text:~/css/tippy.css'
 import scaleCss from 'data-text:~/css/scale.css'
 import { fetchDelTwitterInfo, fetchTwitterInfo } from './services/api'
-import { DraggablePanel } from '~contents/compontents/DraggablePanel.tsx';
+// import { DraggablePanel } from '~contents/compontents/DraggablePanel.tsx';
 import { useStorage } from '@plasmohq/storage/hook'
 import { extractUsernameFromUrl } from '~contents/utils';
 import { useI18n } from '~contents/hooks/i18n.ts';
@@ -23,6 +23,12 @@ export const getStyle = () => {
   style.textContent = cssText + tipCss + scaleCss;
   return style
 }
+
+// insert into page dom
+export const getInlineAnchor = () =>
+  document.querySelector('div[data-testid="UserName"]')
+
+export const getShadowHostId = () => `twitter-panel`
 
 function TwitterPanel() {
   const [showPanel] = useStorage('@settings/showPanel', true);
@@ -102,54 +108,93 @@ function TwitterPanel() {
   if (error || !userId) {
     return <></>
   }
-  return <DraggablePanel
-    width={320}
-    dragHandleClassName="tw-hunt-drag-handle"
+  return <div
+    className={`w-full bg-[#000000] text-white overflow-hidden opacity-100 border-b border-t  border-[#2F3336] mb-4`}
   >
-    <div className="fixed w-[320px]">
-      {/* Panel Content */}
-      {<div
-        className={`absolute top-0 right-0 w-full bg-[#15202b] rounded-2xl shadow-[0_4px_12px_rgba(0,0,0,0.15)] text-white overflow-hidden opacity-100 shadow-[0_8px_24px_rgba(0,0,0,0.25)]`}
-      >
-        {loading && (
-          <div className="absolute inset-0 bg-[#15202b]/70 backdrop-blur-[3px] z-10 flex flex-col items-center justify-center pointer-events-auto">
-            <Loader2 className="w-8 h-8 text-blue-400 animate-spin mb-2" />
-            <p className="text-sm text-blue-200">{t('loading')}</p>
-          </div>
-        )}
-        {/* Sticky Header */}
-        <div className="sticky top-0 z-50 bg-[#15202b]/95 backdrop-blur-sm border-b border-gray-700/50">
-          <div className="absolute right-2 top-2 flex items-center gap-1">
-            <div className="tw-hunt-drag-handle p-1.5 rounded-full hover:bg-gray-700/50 transition-colors cursor-grab active:cursor-grabbing">
-              <GripVertical className="w-4 h-4 text-gray-400" />
-            </div>
-          </div>
-          <div className="p-3 pt-2">
-            <h1 className="text-sm font-semibold pl-1">
-              {`@${userId}`}
-              {userStats?.basicInfo?.isKol && <Tags className="w-4 h-4 ml-4 mb-0.5 text-gray-400 inline-flex" />}
-              {userStats?.basicInfo?.classification && (userStats?.basicInfo?.classification !== 'unknown') &&
-								<span className="text-xs text-gray-400 ml-1">{userStats?.basicInfo?.classification}</span>}
-            </h1>
-          </div>
-        </div>
+    {loading && (
+      <div className="absolute inset-0 bg-[#000000]/70 backdrop-blur-[3px] z-10 flex flex-col items-center justify-center pointer-events-auto">
+        <Loader2 className="w-8 h-8 text-blue-400 animate-spin mb-2" />
+        <p className="text-sm text-blue-200">{t('loading')}</p>
+      </div>
+    )}
+    {/* Sticky Header */}
+    {/*<div className="sticky top-0 z-50 bg-[#15202b]/95 backdrop-blur-sm border-b border-gray-700/50">*/}
+    {/*  <div className="absolute right-2 top-2 flex items-center gap-1">*/}
+    {/*    <div className="tw-hunt-drag-handle p-1.5 rounded-full hover:bg-gray-700/50 transition-colors cursor-grab active:cursor-grabbing">*/}
+    {/*      <GripVertical className="w-4 h-4 text-gray-400" />*/}
+    {/*    </div>*/}
+    {/*  </div>*/}
+    {/*  <div className="p-3 pt-2">*/}
+    {/*    <h1 className="text-sm font-semibold pl-1">*/}
+    {/*      {`@${userId}`}*/}
+    {/*      {userStats?.basicInfo?.isKol && <Tags className="w-4 h-4 ml-4 mb-0.5 text-gray-400 inline-flex" />}*/}
+    {/*      {userStats?.basicInfo?.classification && (userStats?.basicInfo?.classification !== 'unknown') &&*/}
+		{/*				<span className="text-xs text-gray-400 ml-1">{userStats?.basicInfo?.classification}</span>}*/}
+    {/*    </h1>*/}
+    {/*  </div>*/}
+    {/*</div>*/}
 
-        <div className="max-h-[90vh] overflow-y-auto overflow-x-hidden custom-scrollbar">
-          {/* KOL Followers Section */}
-          <KolFollowersSection kolData={userStats} />
+    <div className="max-h-[90vh] overflow-y-auto overflow-x-hidden custom-scrollbar">
+      {/* KOL Followers Section */}
+      <KolFollowersSection kolData={userStats} />
 
-          {/*Token Performance Section*/}
-          {userStats?.basicInfo?.classification === 'person' && userStats.kolTokenMention &&
-						<TokenPerformanceSection kolData={userStats} />}
+      {/*Token Performance Section*/}
+      {userStats?.basicInfo?.classification === 'person' && userStats.kolTokenMention &&
+				<TokenPerformanceSection kolData={userStats} />}
 
-          {/* Deleted Tweets Section */}
-          {(userStats?.basicInfo?.isKol || deletedTweets?.length) ?
-            <DeletedTweetsSection deletedTweets={deletedTweets} loadingDel={loadingDel} /> : null}
-        </div>
-      </div>}
-
+      {/* Deleted Tweets Section */}
+      {(userStats?.basicInfo?.isKol || deletedTweets?.length) ?
+        <DeletedTweetsSection deletedTweets={deletedTweets} loadingDel={loadingDel} /> : null}
     </div>
-  </DraggablePanel>
+  </div>
+  // return <DraggablePanel
+  //   width={320}
+  //   dragHandleClassName="tw-hunt-drag-handle"
+  // >
+  //   <div className="w-[320px]">
+  //     {/* Panel Content */}
+  //     {<div
+  //       className={`absolute top-0 right-0 w-full bg-[#15202b] rounded-2xl shadow-[0_4px_12px_rgba(0,0,0,0.15)] text-white overflow-hidden opacity-100 shadow-[0_8px_24px_rgba(0,0,0,0.25)]`}
+  //     >
+  //       {loading && (
+  //         <div className="absolute inset-0 bg-[#15202b]/70 backdrop-blur-[3px] z-10 flex flex-col items-center justify-center pointer-events-auto">
+  //           <Loader2 className="w-8 h-8 text-blue-400 animate-spin mb-2" />
+  //           <p className="text-sm text-blue-200">{t('loading')}</p>
+  //         </div>
+  //       )}
+  //       {/* Sticky Header */}
+  //       <div className="sticky top-0 z-50 bg-[#15202b]/95 backdrop-blur-sm border-b border-gray-700/50">
+  //         <div className="absolute right-2 top-2 flex items-center gap-1">
+  //           <div className="tw-hunt-drag-handle p-1.5 rounded-full hover:bg-gray-700/50 transition-colors cursor-grab active:cursor-grabbing">
+  //             <GripVertical className="w-4 h-4 text-gray-400" />
+  //           </div>
+  //         </div>
+  //         <div className="p-3 pt-2">
+  //           <h1 className="text-sm font-semibold pl-1">
+  //             {`@${userId}`}
+  //             {userStats?.basicInfo?.isKol && <Tags className="w-4 h-4 ml-4 mb-0.5 text-gray-400 inline-flex" />}
+  //             {userStats?.basicInfo?.classification && (userStats?.basicInfo?.classification !== 'unknown') &&
+  // 							<span className="text-xs text-gray-400 ml-1">{userStats?.basicInfo?.classification}</span>}
+  //           </h1>
+  //         </div>
+  //       </div>
+  //
+  //       <div className="max-h-[90vh] overflow-y-auto overflow-x-hidden custom-scrollbar">
+  //         {/* KOL Followers Section */}
+  //         <KolFollowersSection kolData={userStats} />
+  //
+  //         {/*Token Performance Section*/}
+  //         {userStats?.basicInfo?.classification === 'person' && userStats.kolTokenMention &&
+  // 					<TokenPerformanceSection kolData={userStats} />}
+  //
+  //         {/* Deleted Tweets Section */}
+  //         {(userStats?.basicInfo?.isKol || deletedTweets?.length) ?
+  //           <DeletedTweetsSection deletedTweets={deletedTweets} loadingDel={loadingDel} /> : null}
+  //       </div>
+  //     </div>}
+  //
+  //   </div>
+  // </DraggablePanel>
 }
 
 export default TwitterPanel
